d5d3c27fec45b306b4d78e4a4aca875361c67b6b
Jasper Swart <jasper.g.swart@gmail.com>
2018-02-09 12:27:49 +0100

    added prop types to markdown field BAC-870

--- src/components/MarkdownField.jsx
+++ src/components/MarkdownField.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const MarkdownField = () => (
-  <textarea className="markdown-field">yoo</textarea>
+const MarkdownField = props => (
+  <textarea
+    id={props.id}
+    className="markdown-field"
+    placeholder={props.placeholder}
+    label={props.label}
+  >{props.value}</textarea>
 );
+
+MarkdownField.defaultProps = {
+  label: '',
+  placeholder: '',
+  // schema: {},
+  value: ''
+};
+
+MarkdownField.propTypes = {
+  // autofocus: PropTypes.bool.isRequired,
+  // disabled: PropTypes.bool.isRequired,
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string,
+  placeholder: PropTypes.string,
+  // readonly: PropTypes.bool.isRequired,
+  // required: PropTypes.bool.isRequired,
+  // schema: PropTypes.object,
+  value: PropTypes.string
+};
 
 export default MarkdownField;
