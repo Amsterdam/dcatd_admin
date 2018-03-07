3fa0aeb5604821792869bfb1cf2955db08afd110
Jasper Swart <jasper.g.swart@gmail.com>
2018-03-07 09:20:47 +0100

    added file upload BAC-870

--- src/widgets/File/File.jsx
+++ src/widgets/File/File.jsx
@@ -1,70 +1,136 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import './file.scss';
 
 const apiUrl = `https://${process.env.NODE_ENV !== 'production' ? 'acc.' : ''}api.data.amsterdam.nl/dcatd/files`;
 
-function processFile(files) {
-  const f = files[0];
-  const formData = new FormData();
-  formData.append('distribution', f);
+class File extends Component {
+  constructor(props) {
+    super(props);
 
-  const xhr = new XMLHttpRequest();
-  console.log('starting...');
+    this.state = {
+      file: props.file,
+      loaded: props.loaded,
+      status: props.status,
+      total: props.total,
+      url: props.url,
+      value: props.value
+    };
 
-  xhr.onloadstart = () => {
-    console.log('for real...');
-  };
+    this.processFile = this.processFile.bind(this);
+    this.resetFile = this.resetFile.bind(this);
+  }
 
-  xhr.upload.onprogress = (e) => {
-    console.log('progress', e, e.loaded, e.total);
-  };
-
-  xhr.onload = () => {
-    console.log('FINISH');
-    // upload success
-    if (xhr.readyState === 4 && (xhr.status === 201 || xhr.status === 200 || xhr.status === 0)) {
-      // if your server sends a message on upload sucess,
-      // get it with xhr.responseText
-      console.log('LOCATION IN DATASTORE', xhr.getResponseHeader('Location'));
+  processFile(files) {
+    if (files.length === 0) {
+      return;
     }
-  };
 
-  xhr.open('POST', apiUrl, true);
-  xhr.send(formData);
-}
+    const formData = new FormData();
+    formData.append('distribution', files[0]);
 
-function File(props) {
-  return (
-    <input
-      type="file"
-      id={props.id}
-      className="form-control"
-      name="distribution"
-      label={props.label}
-      required={props.required}
-      disabled={props.disabled}
-      placeholder={props.placeholder}
-      readOnly={props.readonly}
+    const xhr = new XMLHttpRequest();
 
-      onChange={event => processFile(event.target.files)}
-    />
-  );
+    xhr.onloadstart = () => {
+      this.setState({
+        status: 'busy',
+        file: files[0].name
+      });
+    };
+
+    xhr.upload.onprogress = (e) => {
+      this.setState({
+        loaded: e.loaded,
+        total: e.total
+      });
+    };
+
+    xhr.onload = () => {
+      // upload success
+      if (xhr.readyState === 4 && (xhr.status === 201 || xhr.status === 200 || xhr.status === 0)) {
+        this.setState({
+          status: 'done',
+          url: xhr.getResponseHeader('Location')
+        });
+      }
+    };
+
+    xhr.open('POST', apiUrl, true);
+    xhr.send(formData);
+  }
+
+  resetFile() {
+    this.setState({
+      file: '',
+      loaded: 0,
+      total: 0,
+      status: 'idle',
+      url: ''
+    });
+  }
+
+  render() {
+    const { file, loaded, total, url, status } = this.state;
+
+    return (
+      <div className="file">
+        <input
+          type="text"
+          id={this.props.id}
+          className="form-control file__url"
+          label={this.props.label}
+          disabled={this.props.disabled}
+          placeholder={this.props.placeholder}
+          value={url}
+        />
+        <input
+          type="file"
+          className="form-control file__upload"
+          name="distribution"
+          required={this.props.required}
+          disabled={this.props.disabled}
+          readOnly={this.props.readonly}
+
+          onClick={() => this.resetFile()}
+          onChange={event => this.processFile(event.target.files)}
+        />
+        <div className="file__progress-wrapper">
+          <div
+            className="file__progress"
+            style={{ width: `${(total ? ((loaded / total) * 100).toFixed(0) : 0)}%` }}
+          >{file}</div>
+        </div>
+        <div>{status}</div>
+      </div>
+    );
+  }
 }
 
 File.defaultProps = {
+  file: '',
   label: '',
-  placeholder: ''
+  placeholder: '',
+  loaded: 0,
+  status: 'idle',
+  total: 0,
+  url: '',
+  value: ''
 };
 
 File.propTypes = {
   disabled: PropTypes.bool.isRequired,
+  file: PropTypes.string,
   id: PropTypes.string.isRequired,
   label: PropTypes.string,
+  loaded: PropTypes.number,
   placeholder: PropTypes.string.isRequired,
   readonly: PropTypes.bool.isRequired,
-  required: PropTypes.bool.isRequired
+  required: PropTypes.bool.isRequired,
+  status: PropTypes.string,
+  total: PropTypes.number,
+  url: PropTypes.string,
+  value: PropTypes.string
 
   // onChange: PropTypes.func.isRequired
 };
