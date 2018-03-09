50904099d9ae80a58a41a1dc600e639863758ef9
Jasper Swart <jasper.g.swart@gmail.com>
2018-03-09 14:05:55 +0100

    added more file types and file sizes BAC-870

--- src/fields/Resources/Resources.jsx
+++ src/fields/Resources/Resources.jsx
@@ -1,26 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import filesize from 'filesize';
 
 import { dateFormat } from '../../definitions/localization';
 
 import './resources.scss';
 
-const filetypeMap = {
-  'application/json': 'json',
-  'application/octet-stream': 'overig'
-};
-
 function handleAddResource(type) {
   console.log('handleAddResource', type);
 }
 
-function getFileType(mime) {
-  const type = mime.split('/')[1];
-  console.log(mime, type);
+function getFileType(mime, fieldSchema) {
+  const type = fieldSchema.enumNames[fieldSchema.enum.indexOf(mime)];
   return (
     <span className={`resources-type-content-item-file-type
       resources-type-content-item-file-type-${type}`}
-    >{filetypeMap[mime]}
+    >{type || 'overig'}
     </span>
   );
 }
@@ -47,11 +42,12 @@ const Resources = props => (
                 <div className="resources-type-content-item-updated">
                   {dateFormat.formatDate(resource['foaf:isPrimaryTopicOf']['dct:issued'])}</div>
                 <div className="resources-type-content-item-size">
-                  {resource['dcat:byteSize'] > 0 ? (resource['dcat:byteSize'] || 0) / 1024 : ''} kb</div>
+                  {resource['dcat:byteSize'] > 0 ? filesize(resource['dcat:byteSize']) : ''}</div>
                 <div className="resources-type-content-item-title">
                   {resource['dct:title']}</div>
                 <div className="resources-type-content-item-description">
-                  {getFileType(resource['dct:format'])}{resource['dct:description']}</div>
+                  {getFileType(resource['dct:format'], props.schema.items.properties['dct:format'])}
+                  {resource['dct:description']}</div>
               </div>
               <button
                 onClick={() => handleAddResource(type)}
