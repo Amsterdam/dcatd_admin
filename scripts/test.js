a2e9c9144945e0995ec8e998dcb39371a4ecb13c
Jasper Swart <jasper.g.swart@gmail.com>
2018-04-06 15:13:27 +0200

    added watch to jest BAC-1084

--- scripts/test.js
+++ scripts/test.js
@@ -17,10 +17,4 @@ process.env.PUBLIC_URL = '';
 
 const argv = process.argv.slice(2);
 
-// Watch unless on CI or in coverage mode
-// if (!process.env.CI && argv.indexOf('--coverage') < 0) {
-//   argv.push('--watch');
-// }
-
-
 jest.run(argv);
