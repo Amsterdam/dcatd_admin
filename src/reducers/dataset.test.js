76c3c6c442b39c83de101c75ce25db65abbf708c
Jasper Swart <jasper.g.swart@gmail.com>
2018-04-09 11:31:08 +0200

    added test BAC-1084

---
+++ src/reducers/dataset.test.js
@@ -0,0 +1,86 @@
+import dataset from './dataset';
+
+describe('dataset reducer', () => {
+  it.only('should handle initial state', () => {
+    expect(
+      dataset(undefined, {})
+    ).toEqual({});
+  });
+
+  // describe('FETCH_DATASET_SUCCESS', () => {
+  //   it('adds an dataset to an empty state', () => {
+  //     expect(
+  //       dataset([], {
+  //         type: 'FETCH_DATASET_SUCCESS',
+  //         dataset: {
+  //           emailAddress: 'john@doe.com',
+  //           name: 'John Doe',
+  //           id: 0
+  //         }
+  //       })
+  //     ).toEqual([
+  //       {
+  //         emailAddress: 'john@doe.com',
+  //         name: 'John Doe',
+  //         id: 0
+  //       }
+  //     ]);
+  //   });
+  // });
+
+  // it('adds an dataset to a non-empty state', () => {
+  //   expect(
+  //     dataset([
+  //       {
+  //         emailAddress: 'john@doe.com',
+  //         name: 'John Doe',
+  //         active: true,
+  //         id: 0
+  //       }
+  //     ], {
+  //       type: 'FETCH_DATASET_SUCCESS',
+  //       dataset: {
+  //         emailAddress: 'jane@doe.com',
+  //         name: 'Jane Doe',
+  //         id: 1
+  //       }
+  //     })
+  //   ).toEqual([
+  //     {
+  //       emailAddress: 'john@doe.com',
+  //       name: 'John Doe',
+  //       active: true,
+  //       id: 0
+  //     }, {
+  //       emailAddress: 'jane@doe.com',
+  //       name: 'Jane Doe',
+  //       id: 1
+  //     }
+  //   ]);
+  // });
+  //
+  // it('overwrites an existing dataset', () => {
+  //   expect(
+  //     dataset([
+  //       {
+  //         emailAddress: 'john@doe.com',
+  //         name: 'John Doe',
+  //         id: 0
+  //       }
+  //     ], {
+  //       type: 'FETCH_DATASET_SUCCESS',
+  //       dataset: {
+  //         emailAddress: 'john@doe.com',
+  //         name: 'Jane Doe',
+  //         id: 1
+  //       }
+  //     })
+  //   ).toEqual([
+  //     {
+  //       emailAddress: 'john@doe.com',
+  //       name: 'Jane Doe',
+  //       id: 1
+  //     }
+  //   ]);
+  // });
+});
--- src/reducers/datasets.test.js
+++ src/reducers/datasets.test.js
@@ -1,86 +1,28 @@
-// import datasets from './datasets';
-//
-// describe('datasets reducer', () => {
-//   it('should handle initial state', () => {
-//     expect(
-//       datasets(undefined, {}).length
-//     ).toBe(0);
-//   });
-//
-//   describe('FETCH_DATASET_SUCCESS', () => {
-//     it('adds an dataset to an empty state', () => {
-//       expect(
-//         datasets([], {
-//           type: 'FETCH_DATASET_SUCCESS',
-//           dataset: {
-//             emailAddress: 'john@doe.com',
-//             name: 'John Doe',
-//             id: 0
-//           }
-//         })
-//       ).toEqual([
-//         {
-//           emailAddress: 'john@doe.com',
-//           name: 'John Doe',
-//           id: 0
-//         }
-//       ]);
-//     });
-//
-//     it('adds an dataset to a non-empty state', () => {
-//       expect(
-//         datasets([
-//           {
-//             emailAddress: 'john@doe.com',
-//             name: 'John Doe',
-//             active: true,
-//             id: 0
-//           }
-//         ], {
-//           type: 'FETCH_DATASET_SUCCESS',
-//           dataset: {
-//             emailAddress: 'jane@doe.com',
-//             name: 'Jane Doe',
-//             id: 1
-//           }
-//         })
-//       ).toEqual([
-//         {
-//           emailAddress: 'john@doe.com',
-//           name: 'John Doe',
-//           active: true,
-//           id: 0
-//         }, {
-//           emailAddress: 'jane@doe.com',
-//           name: 'Jane Doe',
-//           id: 1
-//         }
-//       ]);
-//     });
-//
-//     it('overwrites an existing dataset', () => {
-//       expect(
-//         datasets([
-//           {
-//             emailAddress: 'john@doe.com',
-//             name: 'John Doe',
-//             id: 0
-//           }
-//         ], {
-//           type: 'FETCH_DATASET_SUCCESS',
-//           dataset: {
-//             emailAddress: 'john@doe.com',
-//             name: 'Jane Doe',
-//             id: 1
-//           }
-//         })
-//       ).toEqual([
-//         {
-//           emailAddress: 'john@doe.com',
-//           name: 'Jane Doe',
-//           id: 1
-//         }
-//       ]);
-//     });
-//   });
-// });
+import datasets from './datasets';
+
+describe('datasets reducer', () => {
+  it('should handle initial state', () => {
+    expect(
+      datasets(undefined, {}).length
+    ).toBe(0);
+  });
+
+  describe('FETCH_DATASETS_SUCCESS', () => {
+    it('fetches a list of datasets', () => {
+      expect(
+        datasets([], {
+          type: 'FETCH_DATASETS_SUCCESS',
+          datasets: [{
+            title: 'dataset 1'
+          }, {
+            title: 'dataset 2'
+          }]
+        })
+      ).toEqual([{
+        title: 'dataset 1'
+      }, {
+        title: 'dataset 2'
+      }]);
+    });
+  });
+});
