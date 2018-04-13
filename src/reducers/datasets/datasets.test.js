8bfa8b29abd12ad230d87641f0f260585bdea437
Jasper Swart <jasper.g.swart@gmail.com>
2018-04-13 12:09:24 +0200

    refactor thunk into sagas part one BAC-1084

---
+++ src/reducers/datasets/datasets.test.js
@@ -0,0 +1,28 @@
+// import datasets from './datasets';
+//
+// describe('datasets reducer', () => {
+//   it('should handle initial state', () => {
+//     expect(
+//       datasets(undefined, {}).length
+//     ).toBe(0);
+//   });
+//
+//   describe('FETCH_DATASETS_SUCCESS', () => {
+//     it('fetches a list of datasets', () => {
+//       expect(
+//         datasets([], {
+//           type: 'FETCH_DATASETS_SUCCESS',
+//           datasets: [{
+//             title: 'dataset 1'
+//           }, {
+//             title: 'dataset 2'
+//           }]
+//         })
+//       ).toEqual([{
+//         title: 'dataset 1'
+//       }, {
+//         title: 'dataset 2'
+//       }]);
+//     });
+//   });
+// });
