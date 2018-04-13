248611252da7e668599f88ac6afa0e2899d646f7
Jasper Swart <jasper.g.swart@gmail.com>
2018-04-13 15:19:49 +0200

    no more check if distributions have changed BAC-1084

--- src/components/DatasetDetail/DatasetDetail.jsx
+++ src/components/DatasetDetail/DatasetDetail.jsx
@@ -86,24 +86,20 @@ class DatasetDetail extends Component {
 
   handleSetResource(resource) {
     this.props.onSetResource(resource);
-    // if (this.hasChanged()) {
-    //   this.props.setModal({
-    //     actionLabel: 'Opslaan en verdergaan',
-    //     cancelLabel: 'Op deze pagina blijven',
-    //     content: 'Wijzigingen op deze pagina zijn nog niet opgeslagen.',
-    //     open: true,
-    //     onProceed: () => {
-    //       // submit form
-    //       // @TODO we have to fix submitting the form at this point
-    //     }
-    //   });
-    // } else {
-    //   this.props.onSetResource(resource);
-    // }
-  }
-
-  hasChanged() {
-    return !isEqual(this.state.dataset, this.props.dataset, ['@context']);
+    if (!isEqual(this.state.dataset, this.props.dataset, ['@context', 'dcat:distribution'])) {
+      this.props.setModal({
+        actionLabel: 'Opslaan en verdergaan',
+        cancelLabel: 'Op deze pagina blijven',
+        content: 'Wijzigingen op deze pagina zijn nog niet opgeslagen.',
+        open: true,
+        onProceed: () => {
+          // submit form
+          // @TODO we have to fix submitting the form at this point
+        }
+      });
+    } else {
+      this.props.onSetResource(resource);
+    }
   }
 
   hasDataset() {
@@ -125,7 +121,7 @@ class DatasetDetail extends Component {
   }
 
   handleCancel() {
-    if (this.hasChanged()) {
+    if (!isEqual(this.state.dataset, this.props.dataset, ['@context'])) {
       this.props.setModal({
         actionLabel: 'De gemaakte wijzigingen negeren',
         cancelLabel: 'Blijf deze dataset bewerken',
