fdec1cde0fb2a6136ae59b135f4801f042eafe9f
Jasper Swart <jasper.g.swart@gmail.com>
2018-04-20 12:44:33 +0200

    added unit test for resource BAC-1084

---
+++ src/reducers/resource/resource.test.js
@@ -0,0 +1,55 @@
+import resource from './resource';
+
+describe('resource reducer', () => {
+  it('should handle initial state', () => {
+    expect(
+      resource(undefined, {})
+    ).toEqual({});
+  });
+
+  describe('SET_RESOURCE_SUCCESS', () => {
+    it('sets the resource state', () => {
+      expect(
+        resource({}, {
+          type: 'SET_RESOURCE_SUCCESS',
+          resource: {
+            '@id': '_:d1',
+            'dct:title': 'Bewoners die zich veilig voelen in hun stad',
+            'dcat:accessURL': 'http',
+            'ams:resourceType': 'data',
+            'ams:distributionType': 'file',
+            'dct:format': 'application/vnd.ms-excel',
+            'ams:classification': 'public'
+          }
+        })
+      ).toEqual({
+        '@id': '_:d1',
+        'dct:title': 'Bewoners die zich veilig voelen in hun stad',
+        'dcat:accessURL': 'http',
+        'ams:resourceType': 'data',
+        'ams:distributionType': 'file',
+        'dct:format': 'application/vnd.ms-excel',
+        'ams:classification': 'public'
+      });
+    });
+  });
+
+  describe('EMPTY_RESOURCE_SUCCESS', () => {
+    it('empties the resource state', () => {
+      expect(
+        resource({}, {
+          type: 'EMPTY_RESOURCE_SUCCESS',
+          resource: {
+            '@id': '_:d1',
+            'dct:title': 'Bewoners die zich veilig voelen in hun stad',
+            'dcat:accessURL': 'http',
+            'ams:resourceType': 'data',
+            'ams:distributionType': 'file',
+            'dct:format': 'application/vnd.ms-excel',
+            'ams:classification': 'public'
+          }
+        })
+      ).toEqual({});
+    });
+  });
+});
