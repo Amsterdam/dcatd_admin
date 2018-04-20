ca801362dfd7bded76d5fd67548da60c64e16abf
Jasper Swart <jasper.g.swart@gmail.com>
2018-04-20 12:50:29 +0200

    added unit test for resourceToDataset BAC-1084

---
+++ src/reducers/resourceToDataset/resourceToDataset.test.js
@@ -0,0 +1,36 @@
+import resourceToDataset from './resourceToDataset';
+
+describe('resource reducer', () => {
+  it('should handle initial state', () => {
+    expect(
+      resourceToDataset(undefined, {})
+    ).toEqual({});
+  });
+
+  describe('SET_RESOURCE_TO_DATASET_SUCCESS', () => {
+    it('sets the resourceToDataset state to pass the edited resource to the dataset', () => {
+      expect(
+        resourceToDataset({}, {
+          type: 'SET_RESOURCE_TO_DATASET_SUCCESS',
+          resource: {
+            '@id': '_:d1',
+            'dct:title': 'Bewoners die zich veilig voelen in hun stad',
+            'dcat:accessURL': 'http',
+            'ams:resourceType': 'data',
+            'ams:distributionType': 'file',
+            'dct:format': 'application/vnd.ms-excel',
+            'ams:classification': 'public'
+          }
+        })
+      ).toEqual({
+        '@id': '_:d1',
+        'dct:title': 'Bewoners die zich veilig voelen in hun stad',
+        'dcat:accessURL': 'http',
+        'ams:resourceType': 'data',
+        'ams:distributionType': 'file',
+        'dct:format': 'application/vnd.ms-excel',
+        'ams:classification': 'public'
+      });
+    });
+  });
+});
