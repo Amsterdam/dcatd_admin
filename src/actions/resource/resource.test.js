ff13fabdbe6dd0e8556603ecde18fd22eb25c72a
Jasper Swart <jasper.g.swart@gmail.com>
2018-04-20 15:45:36 +0200

    added unit test for resource actions BAC-1084

---
+++ src/actions/resource/resource.test.js
@@ -0,0 +1,51 @@
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { setResource, emptyResource } from './resource';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+describe('resource actions', () => {
+  it('should dispatch setResource', () => {
+    const expectedActions = [
+      {
+        type: 'SET_RESOURCE_SUCCESS',
+        resource: {
+          '@id': '_:d1',
+          'dct:title': 'Bewoners die zich veilig voelen in hun stad',
+          'dcat:accessURL': 'http',
+          'ams:resourceType': 'data',
+          'ams:distributionType': 'file',
+          'dct:format': 'application/vnd.ms-excel',
+          'ams:classification': 'public'
+        }
+      }
+    ];
+
+    const store = mockStore();
+    store.dispatch(setResource({
+      '@id': '_:d1',
+      'dct:title': 'Bewoners die zich veilig voelen in hun stad',
+      'dcat:accessURL': 'http',
+      'ams:resourceType': 'data',
+      'ams:distributionType': 'file',
+      'dct:format': 'application/vnd.ms-excel',
+      'ams:classification': 'public'
+    }));
+
+    expect(store.getActions()).toEqual(expectedActions);
+  });
+
+  it('should dispatch setUiDatasetOrder', () => {
+    const expectedActions = [
+      {
+        type: 'EMPTY_RESOURCE_SUCCESS'
+      }
+    ];
+
+    const store = mockStore();
+    store.dispatch(emptyResource());
+
+    expect(store.getActions()).toEqual(expectedActions);
+  });
+});
