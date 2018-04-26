6380668e5aa3d6210f650b5a0923d5efc8b62166
Jasper Swart <jasper.g.swart@gmail.com>
2018-04-26 16:33:00 +0200

    added unit test for resource detail BAC-1084

--- src/components/ResourceDetail/ResourceDetail.test.jsx
+++ src/components/ResourceDetail/ResourceDetail.test.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import ResourceDetail from './ResourceDetail';
+import schema from './schema.json';
 
 describe('The ResourceDetail component', () => {
   let wrap;
@@ -9,19 +10,6 @@ describe('The ResourceDetail component', () => {
   let onEmptyResourceSpy;
   let setModalSpy;
 
-  const mockSchema = {
-    type: 'array',
-    default: [],
-    items: {
-      properties: {
-        'ams:resourceType': {
-          enum: ['data', 'doc', 'vis', 'app'],
-          enumNames: ['Data', 'Documentatie', 'Visualisatie', 'Voorbeeldtoepassing']
-        }
-      }
-    }
-  };
-
   beforeEach(() => {
     setModalSpy = jest.fn();
     onEmptyResourceSpy = jest.fn();
@@ -29,7 +17,7 @@ describe('The ResourceDetail component', () => {
     datasetTitle = '';
     wrap = shallow((
       <ResourceDetail
-        schema={mockSchema}
+        schema={schema}
         uiResource={{}}
         formData={{}}
         datasetTitle={datasetTitle}
@@ -47,37 +35,8 @@ describe('The ResourceDetail component', () => {
   });
 
   // describe('triggering events', () => {
-  //   let setResourceDetailSpy;
-  //   let onProceedSpy;
-  //
-  //   beforeEach(() => {
-  //     setResourceDetailSpy = jest.fn();
-  //     onProceedSpy = jest.fn();
-  //   });
-  //
-  //   it('clicking submit closes modal and triggers onProceed', () => {
-  //     wrap.setProps({
-  //       modal: {
-  //         onProceed: onProceedSpy
-  //       },
-  //       setResourceDetail: setResourceDetailSpy
-  //     });
-  //
-  //     wrap.find('.dcatd-form-button-submit').simulate('click');
-  //
-  //     expect(onProceedSpy).toHaveBeenCalled();
-  //     expect(setResourceDetailSpy).toHaveBeenCalledWith({ open: false });
-  //   });
-  //
-  //   it('clicking cancel closes modal', () => {
-  //     wrap.setProps({
-  //       setResourceDetail: setResourceDetailSpy
-  //     });
-  //
-  //     wrap.find('.dcatd-form-button-cancel').simulate('click');
-  //
-  //     expect(onProceedSpy).not.toHaveBeenCalled();
-  //     expect(setResourceDetailSpy).toHaveBeenCalledWith({ open: false });
+  //   it('clicking cancel opens modal', () => {
+  //     console.log('--------------', wrap.find('.resource-title'));
   //   });
   // });
 });
