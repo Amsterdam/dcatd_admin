9c8a2a3d934f3ae9dbdafe224cdd5412357b7601
Jasper Swart <jasper.g.swart@gmail.com>
2018-04-30 10:57:08 +0200

    updated jest config BAC-1084

--- config/jest.js
+++ config/jest.js
@@ -2,24 +2,39 @@
 
 import Enzyme, { shallow, render, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import jsdom from 'jsdom';
+import { JSDOM } from 'jsdom';
 
 // React 16 Enzyme adapter
 Enzyme.configure({ adapter: new Adapter() });
+
 
 // Make Enzyme functions available in all test files without importing
 global.shallow = shallow;
 global.mount = mount;
 global.render = render;
 
+
 // load the dom
-const { JSDOM } = jsdom;
-const { document } = (new JSDOM('<!doctype html><html><body></body></html>')).window;
-global.document = document;
-global.window = document.defaultView;
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
+const { window } = jsdom;
+
+function copyProps(src, target) {
+  const props = Object.getOwnPropertyNames(src)
+    .filter(prop => typeof target[prop] === 'undefined')
+    .reduce((result, prop) => ({
+      ...result,
+      [prop]: Object.getOwnPropertyDescriptor(src, prop),
+    }), {});
+  Object.defineProperties(target, props);
+}
+
+global.window = window;
+global.document = window.document;
 global.navigator = {
-  userAgent: 'node.js'
+  userAgent: 'node.js',
 };
+// copyProps(window, global);
+
 
 // add global fetch
 global.fetch = require('jest-fetch-mock');
