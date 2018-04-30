bddcd2bba61775c9e0233ef3de2821f0882574df
Jasper Swart <jasper.g.swart@gmail.com>
2018-04-30 13:31:50 +0200

    added store to dataset detail BAC-1084

--- src/components/DatasetDetail/DatasetDetail.test.jsx
+++ src/components/DatasetDetail/DatasetDetail.test.jsx
@@ -1,25 +1,32 @@
 import React from 'react';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 
 import DatasetDetail from './DatasetDetail';
-import schema from './schema.json';
+import schema from './__mocks__/schema.json';
+import uiDataset from './__mocks__/uiDataset';
+
+const middlewares = [thunk];
 
 describe('The DatasetDetail component', () => {
+  let store;
   let wrap;
   let datasetTitle;
   let setModalSpy;
 
   beforeEach(() => {
+    store = configureMockStore(middlewares)();
     setModalSpy = jest.fn();
     datasetTitle = '';
     wrap = shallow((
       <DatasetDetail
         schema={schema}
-        uiDataset={{}}
+        uiDataset={uiDataset}
         formData={{}}
         datasetTitle={datasetTitle}
         setModal={setModalSpy}
       />
-    ));
+    ), { context: { store } }).dive();
   });
 
   describe('renders component', () => {
---
+++ src/components/DatasetDetail/__mocks__/uiDataset.js
@@ -0,0 +1,109 @@
+// import dayPickerProps from './day-picker';
+// rte has to be fixed in react-jsonschema-form-extras
+// import rte from './rte';
+
+export default {
+  'ui:field': [
+    'resources',
+    'dropdown',
+    'rte'
+  ],
+  'ui:widget': [
+    'markdown'
+  ],
+
+  'dcat:contactPoint': {
+    'ui:order': [
+      'vcard:fn',
+      'vcard:hasEmail',
+      'vcard:hasURL'
+    ],
+
+    'vcard:hasEmail': {
+      'ui:placeholder': 'jan@amsterdam.nl'
+    },
+    'vcard:hasURL': {
+      'ui:placeholder': 'http://'
+    }
+  },
+  'dcat:distribution': {
+    'ui:field': 'resources'
+  },
+  'dct:description': {
+    'ui:widget': 'markdown'
+    // 'ui:field': 'rte',
+    // rte
+  },
+  'dcat:landingPage': {
+    'ui:placeholder': 'http://'
+  },
+  'dct:temporal': {
+    'time:hasBeginning': {
+      // 'ui:field': 'rdp',
+      // rdp: {
+      //   placeholder: 'Kies een datum',
+      //   dayPickerProps
+      // }
+    },
+    'time:hasEnd': {
+      // 'ui:field': 'rdp',
+      // rdp: {
+      //   placeholder: 'Kies een datum',
+      //   dayPickerProps
+      // }
+    }
+  },
+  'ams:owner': {
+    'ui:field': 'dropdown',
+    'ui:placeholder': '',
+    'ui:options': {
+      allowAdditions: true,
+      multiple: false
+    }
+  },
+  'dcat:keyword': {
+    'ui:field': 'dropdown',
+    'ui:placeholder': '',
+    'ui:options': {
+      allowAdditions: true,
+      multiple: true
+    }
+  },
+  'dct:publisher': {
+    'ui:order': [
+      'foaf:name',
+      'foaf:mbox',
+      'foaf:homepage'
+    ],
+
+    'foaf:homepage': {
+      'ui:placeholder': 'http://'
+    },
+    'foaf:mbox': {
+      'ui:placeholder': 'jan@amsterdam.nl'
+    }
+  },
+  'dct:identifier': {
+    'ui:widget': 'hidden'
+  },
+  'dcat:theme': {
+    'ui:field': 'dropdown',
+    'ui:placeholder': '',
+    'ui:options': {
+      allowAdditions: false,
+      multiple: true
+    }
+  },
+  'foaf:isPrimaryTopicOf': {
+    'dct:issued': {
+      // 'ui:field': 'rdp',
+      // rdp: {
+      //   placeholder: 'Kies een datum',
+      //   dayPickerProps
+      // }
+    },
+    'dct:modified': {
+      'ui:widget': 'hidden'
+    }
+  }
+};
