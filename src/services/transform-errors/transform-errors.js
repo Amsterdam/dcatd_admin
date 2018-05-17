12842e64414ff3344e2378727a5874cd363911fb
Jasper Swart <jasper.g.swart@gmail.com>
2018-05-17 12:02:15 +0200

    now ignores unmatched error messages BAC-1300

--- src/services/transform-errors/transform-errors.js
+++ src/services/transform-errors/transform-errors.js
@@ -4,7 +4,10 @@ export default function (errors) {
   return errors.map((error) => {
     if (errorsMap[error.message]) {
       error.message = errorsMap[error.message];
+
+      return error;
     }
-    return error;
+
+    return {};
   });
 }
