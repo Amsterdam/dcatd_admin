88ddfc6e4a235bf5254b7feee67aab436a8abe1a
Adrian Tudorache <a.p.tudorache@gmail.com>
2019-01-22 12:58:24 +0100

    DP-6548 clear the datasets cache when something is changed

--- src/services/redirect-to-portal/redirect-to-portal.js
+++ src/services/redirect-to-portal/redirect-to-portal.js
@@ -1,8 +1,12 @@
+import api from '../api/api';
+
 export default function (type = 'detail') {
   const url = window.sessionStorage.getItem(type === 'detail' ? 'DCATD_DETAIL_REDIRECT_URL' : 'DCATD_LIST_REDIRECT_URL');
 
   window.sessionStorage.removeItem('DCATD_DETAIL_REDIRECT_URL');
   window.sessionStorage.removeItem('DCATD_LIST_REDIRECT_URL');
+  api.resetEndpointCache();
 
   window.location.assign(url || '#/datasets');
 }
+
