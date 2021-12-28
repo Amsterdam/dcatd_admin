f5ff4c9df1ae7ea2c5d2822dc3a04f5059d902b1
Thomas Mills <mills.thoma@gmail.com>
2021-12-28 09:59:38 +0100

    Working keycloak auth flow

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,17 +2,11 @@ import React from 'react';
 import { Route, Redirect } from 'react-router';
 import { Container } from 'semantic-ui-react';
 
-import { useKeycloak } from '@react-keycloak/web';
 import Header from '../Header/Header';
 import DatasetsContainer from '../../containers/DatasetsContainer/DatasetsContainer';
 import Modal from '../Modal/Modal';
 
 const App = () => {
-  const { keycloak, initialized } = useKeycloak();
-  if (!initialized && !keycloak.authenticated) {
-    // eslint-disable-next-line react/button-has-type
-    return <button onClick={() => keycloak.login()}>Login</button>;
-  }
   return (
     <div>
       <Header />
