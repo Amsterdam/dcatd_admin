2cfefe5913b9ced6d18416630872d75a23565a14
Thomas Mills <mills.thoma@gmail.com>
2021-12-28 09:59:38 +0100

    Working keycloak auth flow

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,6 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
 import { Provider } from 'react-redux';
 import { createBrowserHistory as createHistory } from 'history';
-import { ReactKeycloakProvider } from '@react-keycloak/web';
 import { HashRouter } from 'react-router-dom';
 import { routerMiddleware } from 'react-router-redux';
 import thunk from 'redux-thunk';
@@ -14,7 +13,7 @@ import App from './components/App/App';
 import { fetchSchema } from './actions/schema/schema';
 import { fetchUiDataset } from './actions/uiDataset/uiDataset';
 import { fetchUiResource } from './actions/uiResource/uiResource';
-import keycloak from './keycloak';
+import { initAuth } from './services/auth/auth';
 import './styling/config.scss';
 import './index.scss';
 
@@ -30,24 +29,17 @@ const store = createStore(
   )
 );
 
-store.dispatch(fetchSchema());
-store.dispatch(fetchUiDataset());
-store.dispatch(fetchUiResource());
-render(
-  <ReactKeycloakProvider
-    authClient={keycloak}
-    initOptions={{
-      checkLoginIframe: false,
-      pkceMethod: 'S256',
-      onLoad: 'check-sso',
-      silentCheckSsoRedirectUri: `${window.location.origin}/`
-    }}
-  >
+initAuth().then(() => {
+  store.dispatch(fetchSchema());
+  store.dispatch(fetchUiDataset());
+  store.dispatch(fetchUiResource());
+  render(
     <Provider store={store}>
       <HashRouter>
         <App />
       </HashRouter>
-    </Provider>
-  </ReactKeycloakProvider>,
-  document.getElementById('root')
-);
+    </Provider>,
+    document.getElementById('root')
+  );
+  document.replaceChild(document, document.documentElement);
+});
